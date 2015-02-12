a618ebc9be0be4c13c55b6d36d6d8412c8fbf480
Kul Subedi <subedi.kulprasad@gmail.com>
2015-02-12 11:40:53 -0600

    Added

---
+++ cprogrammming/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+printf("\nHi Welcome to Linux World!");
+return 0;
+}
+
---
+++ cprogrammming/hello.s
@@ -0,0 +1,28 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"\nHi Welcome to Linux World!"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %eax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-11)"
+	.section	.note.GNU-stack,"",@progbits
