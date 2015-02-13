5ec867ec1220de570c8fbd91c96cded9ca129af8
Kul Subedi <subedi.kulprasad@gmail.com>
2015-02-13 15:06:17 -0600

    CPorg

---
+++ cprogrammming/datatype1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc, char **argv){
+unsigned char c = 200;
+int i = 1000;
+printf("i/c=%d\n",i/c);
+return 0;
+}
+
--- cprogrammming/wraparound.c
+++ cprogrammming/wraparound.c
@@ -6,8 +6,10 @@ int main(int argc, char **argv){
 unsigned int ui;
 
 ui = UINT_MAX;
-ui++;
 printf("\nui=%u",ui);
+printf("\nWe are going to add 1 to ui:");
+ui++;
+printf("\nui=%u\n",ui);
 
 return EXIT_SUCCESS;
 
