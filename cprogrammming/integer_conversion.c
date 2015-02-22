2dee18807bc179a85fba2763d94772e2d631b8a8
Kul Subedi <subedi.kulprasad@gmail.com>
2015-02-22 12:17:25 -0600

    AddeD

---
+++ cprogrammming/integer_conversion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+int main(void){
+	unsigned int ui = UINT_MAX;
+	signed char c = -1;
+	if(c == ui) {
+		printf("-1 = 4,294,967,295?\n");
+	}
+	return EXIT_SUCCESS;
+}
+
