118a94aafcbd93bd849e0edce51e930d34b12973
Kul Subedi <subedi.kulprasad@gmail.com>
2017-11-08 11:24:54 -0600

    Update code_sample.py

--- privilege_escalation/code_sample.py
+++ privilege_escalation/code_sample.py
@@ -5,3 +5,14 @@ import servicemanager
 import win32serviceutil
 import win32service
 
+class Service(win32serviceutil.ServiceFramework):
+    _svc_name_ = 'ScsiAccess'
+    _svc_display_name_ = 'ScsiAccess'
+    
+    
+    def __init__(self, *args):
+        win32serviceutil.ServiceFramework.__init__(self, *args)
+        
+    def sleep(self, sec):
+        win32api.Sleep(sec*1000, True)
+        
