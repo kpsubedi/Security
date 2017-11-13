aa451a1bf05d1c8418bd63380a3546f9219e65e1
Kul Subedi <subedi.kulprasad@gmail.com>
2017-11-13 17:10:14 -0600

    Update findos.py

--- python_script/findos.py
+++ python_script/findos.py
@@ -1,3 +1,72 @@
+Discovery Tools
+===============
+1. Passive 
+2. Active
+
+
+Layer 2 discovery
+*****************
+# arping <target address>
+# arping 192.168.10.15
+
+Send one packet count, c = 1
+
+# arping 192.168.10.15 -c 1
+
+Wireshark
+=========
+Filter out only arp packets
+
+eth.type == 0x0806
+
+Using NMAP
+==========
+# nmap -PR -sn 192.168.10.1-254
+
+# netdiscover -r 192.168.10.0/24 
+
+Passive discovery using -p switch
+
+# netdiscover -p -r 192.168.10.0/24
+
+Layer 3 Discovery
+******************
+# fping -h
+# fping -a -c 2 192.168.10.20
+
+# fping -a -c 2 192.168.10.1 192.168.10.254
+
+# cat > iplist.org
+192.168.10.20
+192.168.10.15
+
+# fping -f /root/iplist.org
+
+# hping3 -h
+
+# hping3 -1 192.168.10.20
+
+# hping3 -1 192.168.10.20 -c 1
+
+using ping we can build ping swepper.
+# ping -c 1 192.168.10.20
+# ping -c 1 102.168.10.200
+
+Notice "bytes from" in ping response.
+# ping -c 1 192.168.10.20 | grep "bytes from"
+
+# ping -c 1 192.168.10.200 | grep "bytes from"
+
+# ping ping -c 1 192.168.10.20 | grep "bytes from" | cut -d " " -f4
+# ping ping -c 1 192.168.10.20 | grep "bytes from" | cut -d " " -f4 | cut -d":" -f1
+
+#!/bin/bash
+
+for i in $(seq 1 20); do
+ping -c 1 192.168.20.$i | grep "bytes from" | cut -d" " -f 4 | cut -d":" -f1 &
+done
+
+
 #scapy
 
 >>> IP()
