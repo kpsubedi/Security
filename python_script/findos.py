63441cbd64486f0e663a2b481ac9102e3432bdde
Kul Subedi <subedi.kulprasad@gmail.com>
2017-11-13 16:48:20 -0600

    Update findos.py

--- python_script/findos.py
+++ python_script/findos.py
@@ -1,3 +1,37 @@
+#scapy
+
+>>> IP()
+>>> ip = IP()
+>>> ip.display()
+
+>>> ip.dst = "192.168.15.20"
+
+To verify the changes were made:
+>>> ip.display()
+
+create an ICMP object which is ping
+>>> ping = ICMP()
+>>> ping.display()
+
+Send one packet to Window System.
+
+>>> window = sr1(ip/ping)
+
+>>> window.display()
+
+set the destination IP to linux machine
+>>> ip.dst = "192.168.15.22"
+>>> linux = sr1(ip/ping)
+
+>>> linux.display()
+
+Window starts ttl value to 128 and linux start to 64. In each router it will decrement by 1.
+
+>>> exit()
+
+
+
+
 #ip:141.225.60.143
 
 
@@ -9,9 +43,9 @@ ping = ICMP()
 ip.dst = ans
 reply = sr1(ip/ping)
 if reply.ttl < 65:
-    os = "Linux Box"
+    os = "Linux Operating System"
 else:
-    os = "Windows Box"
+    os = "Windows Operating System"
 
 print "Operating System is: %s" %os
 
