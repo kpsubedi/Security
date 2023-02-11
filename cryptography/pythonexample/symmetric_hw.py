ce0aa34dec7c671d7f0bddec0efd4a6f32e84975
Kul Subedi <subedi.kulprasad@gmail.com>
2023-02-11 11:46:04 -0800

    Update symmetric_hw.py

--- cryptography/pythonexample/symmetric_hw.py
+++ cryptography/pythonexample/symmetric_hw.py
@@ -1,1 +1,11 @@
 
+""" Symmetric Cryptography Problem 
+
+In this problem you will use modes of AES Symmetric algorithm to encrypt and decrypt the messages. AES supportes 
+different modes of operation: ECB, CBC, CTR.
+
+Problems:
+1. We will use AES-CBC mode to perform encryption and decryption of the input data.
+2. We will use AES-CTR mode to perform encryption and decryption of the input data.
+
+ECB mode of operation example is here: 
