77175e3f03425f329cb8ca8e7572a8a2c3580ab6
Kul Subedi <subedi.kulprasad@gmail.com>
2023-02-11 11:47:41 -0800

    Update symmetric_hw.py

--- cryptography/pythonexample/symmetric_hw.py
+++ cryptography/pythonexample/symmetric_hw.py
@@ -8,4 +8,9 @@ Problems:
 1. We will use AES-CBC mode to perform encryption and decryption of the input data.
 2. We will use AES-CTR mode to perform encryption and decryption of the input data.
 
-ECB mode of operation example is here: 
+ECB mode of operation example is here: https://github.com/kpsubedi/Security/blob/master/cryptography/pythonexample/symmetric_demo.py
+
+To solve these problmes, you need to go through the api documentation here: https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/#cryptography.hazmat.primitives.ciphers.Cipher
+"""
+
+
